9396c5beb4800101b945110a7eb86283190454a5
Tanya Jain <tanya.jain27may@gmail.com>
2022-01-11 06:44:09 +0530

    refactor(component): refactor about page

--- components/About.vue
+++ components/About.vue
@@ -5,14 +5,22 @@
     class="about atleast-full-page container"
   >
     <div data-scroll class="is-inview about-header">
-      <AboutHeader />
-      <!--img :src="images.sample" loop=infinite /-->
+      <!-- <AboutHeader /> -->
+      <!-- <img :src="images.sample" loop=infinite/> -->
+      <img class="about-header-img" :src="images.sample" />
     </div>
     <div class="row">
       <div class="col-md-9 about-text">
         <div class="section-text about-text text-bounding-box">
-          <div v-for="(paragraph, index) in about_list" :key="index">
-            <p v-html="paragraph" />
+          <div>
+            <div v-for="(point, index) in about_list" :key="index">
+              {{ point }}
+            </div>
+          </div>
+          <div class="about-detail">
+            <div v-for="(paragraph, index) in about_detail" :key="index">
+              <p v-html="paragraph" />
+            </div>
           </div>
         </div>
       </div>
@@ -28,9 +36,10 @@ export default {
   data() {
     return {
       images: {
-        sample: require('@/static/gifs/about.gif'),
+        sample: require('@/static/img/about.png'),
       },
       about_list: '',
+      about_detail: '',
     }
   },
   created() {
@@ -39,6 +48,7 @@ export default {
   methods: {
     fetch_about_data() {
       this.about_list = this.$store.state.modules.about.about.list
+      this.about_detail = this.$store.state.modules.about.about.in_detail
     },
   },
 }
@@ -46,6 +56,26 @@ export default {
 
 <style lang="scss">
 .about-text {
-  font-size: 20px;
+  font-size: 1.2em;
+  padding: 0 10px;
+}
+.about-detail {
+  padding-top: 20px;
+}
+.about-header-img {
+  width: 40vw;
+}
+@media screen and (max-width: 400px) {
+  .about-header-img {
+    width: 90vw;
+  }
+  .about-text {
+    font-size: 1em;
+  }
+}
+@media screen and (min-width: 400px) and (max-width: 550px) {
+  .about-header-img {
+    width: 60vw;
+  }
 }
 </style>
